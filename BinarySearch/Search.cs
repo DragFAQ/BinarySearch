7367d0f6495dca172e173318e772358831608173
Drag <drag.faq@gmail.com>
2017-12-30 19:06:05 +0200

    init

---
+++ BinarySearch/Search.cs
@@ -0,0 +1,26 @@
+﻿using BinarySearch.SearchEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearch
+{
+    public static class Search
+    {
+        public static int SearchItem(int[] array, int value)
+        {
+            Interval interval = new Interval(array, 0, array.Length - 1);
+
+            searchValue(interval, value);
+
+            return -1;
+        }
+
+        private static void searchValue(Interval interval, int value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BinarySearch/SearchEngine/Interval.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearch.SearchEngine
+{
+    class Interval
+    {
+        private int[] values;
+        private int firstIndex;
+        private int lastIndex;
+
+        public Interval(int[] array, int firstIndex, int lastIndex)
+        {
+            values = array;
+            this.firstIndex = firstIndex;
+            this.lastIndex = lastIndex;
+        }
+
+        public int getHalfIndex()
+        {
+            return (lastIndex - firstIndex) / 2 + firstIndex;
+        }
+    }
+}
