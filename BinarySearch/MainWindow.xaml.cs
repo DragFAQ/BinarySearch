2c349ae31290865af6402bda8ec43653f1f3c191
Drag <drag.faq@gmail.com>
2018-01-01 17:13:49 +0200

    UI for array and search value

--- BinarySearch/MainWindow.xaml.cs
+++ BinarySearch/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -27,7 +28,27 @@ namespace BinarySearch
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            //webBrowser.     Search.SearchItem();
+            string[] separators = { ",", " " };
+            string[] strArray = tbArray.Text.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            int[] intArray = Array.ConvertAll(strArray, s => int.Parse(s));
+
+            int result = Search.SearchItem(intArray, int.Parse(tbValue.Text));
+            System.Windows.MessageBox.Show(Convert.ToString(result));
+        }
+
+        private void NumberValidationTextBox(object sender, TextChangedEventArgs e)
+        {
+            string s = Regex.Replace(((TextBox)sender).Text, @"[^\d.]", "");
+            ((TextBox)sender).Text = s;
+        }
+
+        private void OnKeyDownHandler(object sender, KeyEventArgs e)
+        {
+            var keyEnum = (System.Windows.Input.Key)e.Key;
+
+            e.Handled = !(keyEnum >= System.Windows.Input.Key.D0 && keyEnum <= System.Windows.Input.Key.D9 ||
+                keyEnum >= System.Windows.Input.Key.NumPad0 && keyEnum <= System.Windows.Input.Key.NumPad9 ||
+                keyEnum == System.Windows.Input.Key.Back || keyEnum == System.Windows.Input.Key.OemComma);
         }
     }
 }
